811a7367327e8d698863d5169e5465b72ccdf4f8
Jaeman Choi <claztec@gmail.com>
2014-06-06 15:18:32 +0900

    1.2 DAO 분리 1.2.2 중복 코드의 메소드 추출.

--- SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDao.java
+++ SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDao.java
@@ -11,34 +11,31 @@ import net.claztec.springbook.user.domain.User;
 public class UserDao {
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/springbook", "spring", "book");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c
 				.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
-		
+
 		ps.executeUpdate();
-		
+
 		ps.close();
 		c.close();
 	}
 
-	public User get(String id) throws SQLException, ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/springbook", "spring", "book");
 
+	public User get(String id) throws SQLException, ClassNotFoundException {
+		Connection c = getConnection();
+		
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
 		rs.next();
-		
+
 		User user = new User();
 		user.setId(rs.getString("id"));
 		user.setName(rs.getString("name"));
@@ -47,22 +44,30 @@ public class UserDao {
 		rs.close();
 		ps.close();
 		c.close();
-		
+
 		return user;
-		
+
 	}
-	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook",
+				"spring", "book");
+		return c;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException,
+			SQLException {
 		UserDao dao = new UserDao();
 		User user = new User();
 		user.setId("claztec");
 		user.setName("최재만");
 		user.setPassword("pass");
-		
+
 		dao.add(user);
-		
+
 		System.out.println(user.getId() + " 등록 성공.");
-		
+
 		User user2 = dao.get(user.getId());
 		System.out.println(user2.getName());
 	}
