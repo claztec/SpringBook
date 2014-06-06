6e0d9b62ac7f299061f41a049101d5fb45e2af46
Jaeman Choi <claztec@gmail.com>
2014-06-06 16:20:44 +0900

    1.4.2 오브젝트 팩토리의 활용 여러개의 Dao를 사용하는 경우. extract method로 중복 제거.

--- SpringBook/src/main/java/net/claztec/springbook/user/dao/DaoFactory.java
+++ SpringBook/src/main/java/net/claztec/springbook/user/dao/DaoFactory.java
@@ -3,9 +3,21 @@ package net.claztec.springbook.user.dao;
 public class DaoFactory {
 
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
+		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
 	}
+	
+//	public AccountDao accountDao() {
+//		AccountDao accountDao = new AccountDao(connectionMaker());
+//		return accountDao;
+//	}
 
+//	public MessageDao messageDao() {
+//		MessageDao messageDao = new MessageDao(connectionMaker());
+//		return messageDao;
+//	}
+
+	private ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
 }
