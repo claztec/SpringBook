7b49a01c0f379bd51947f9d96531da7769105f7e
Jaeman Choi <claztec@gmail.com>
2014-06-06 16:16:01 +0900

    1.4.1 오브젝트 팩토리 UserDaoTest.java에서 Connection과 UserDao를 결합하는 것은 사실 Factory에서 해야 하는 일이다. UserDaoTest는 단지 테스트만 하면 된다.

--- SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDaoTest.java
+++ SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDaoTest.java
@@ -8,9 +8,8 @@ public class UserDaoTest {
 	
 	public static void main(String[] args) throws ClassNotFoundException,
 			SQLException {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
 		
-		UserDao dao = new UserDao(connectionMaker);
+		UserDao dao = new DaoFactory().userDao();
 		User user = new User();
 		user.setId("claztec");
 		user.setName("최재만");
