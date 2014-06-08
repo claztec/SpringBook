496d074cb3f6159956563a5eeffe42bad8f144be
Jaeman Choi <claztec@gmail.com>
2014-06-08 17:13:20 +0900

    2.4.1 테스트를 위한 애플리케션 컨텍스트 관리 ApplicationContext를 AutoWired 스프링 테스트 컨텍스트 프레임워크 적용. (pom.xml 수정 - spring boot와 버전 충돌 3.x와 4.x)

--- SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDaoTest.java
+++ SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDaoTest.java
@@ -9,11 +9,20 @@ import net.claztec.springbook.user.domain.User;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
+	
+	@Autowired
+	private ApplicationContext context;
 	
 	private UserDao dao;
 	
@@ -23,8 +32,7 @@ public class UserDaoTest {
 	
 	@Before
 	public void setUp() {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		dao = context.getBean("userDao", UserDao.class);
+		this.dao = this.context.getBean("userDao", UserDao.class);
 		
 		user1 = new User("gyumee", "박성철", "springno1");
 		user2 = new User("leeqw700", "이길원", "springno2");
