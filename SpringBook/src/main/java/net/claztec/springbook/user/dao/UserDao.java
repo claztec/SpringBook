c3e94f535c2f4151af64e086707afe97ae691419
Jaeman Choi <claztec@gmail.com>
2014-06-09 23:51:02 +0900

    3.1.1 예외처리 기능을 갖춘 DAO deleteAll(), getCount()에 try~catch 예외처리.

--- SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDao.java
+++ SpringBook/src/main/java/net/claztec/springbook/user/dao/UserDao.java
@@ -64,28 +64,72 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("delete from users");
-		ps.execute();
-		
-		ps.close();
-		c.close();
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("delete from users");
+			ps.execute();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					
+				}
+			}
+			
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+					
+				}
+			}
+		}
 	}
 	
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+				
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select count(*) from users");
+			
+			rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					
+				}
+			}
+			
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					
+				}
+			}
+			
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+					
+				}
+			}
+		}
 		
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count;
 	}
 }
